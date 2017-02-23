52d081aa09c3a34f5b9d6da84959789faf4216f8
Daniel Colangelo <Danalfons@yahoo.com>
2017-02-23 00:58:55 -0500

    interface and starting code
    started some code after creating the interface.

---
+++ is it prime/is it prime/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  is it prime
+//
+//  Created by Danny Colangelo on 2/22/17.
+//  Copyright © 2017 Danny Colangelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var numberField: UITextField!
+    @IBOutlet weak var answerField: UILabel!
+    @IBAction func findOutButton(_ sender: Any) {
+        
+        
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
