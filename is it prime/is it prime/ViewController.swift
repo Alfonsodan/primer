520da1fe318a2892a3ae29d7e81daa40fff1bf97
Daniel Colangelo <Danalfons@yahoo.com>
2017-02-23 01:56:35 -0500

    finished the code
    finished the code but there is still a bug with
    the optional statement… ??

--- is it prime/is it prime/ViewController.swift
+++ is it prime/is it prime/ViewController.swift
@@ -12,9 +12,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var numberField: UITextField!
     @IBOutlet weak var answerField: UILabel!
     @IBAction func findOutButton(_ sender: Any) {
-        
-        
-        
+        if let userEnteredString = numberField.text {
+            let userEnteredString = Int(numberField.text!)
+            if let number = userEnteredString {
+                var isPrime = true
+                if number == 1 {
+                    isPrime = false
+                }
+                var i = 1
+                while i < number {
+                    if number % i == 0 {
+                        isPrime = false
+                    }
+                    i += 1
+                }
+                if isPrime {
+                    answerField.text = "\(number) is prime!"
+                } else {
+                    answerField.text = "\(number) is not prime"
+                }
+            } else {
+                answerField.text = "please enter a positive whole number!"
+            }
+        }
     }
 
     override func viewDidLoad() {
